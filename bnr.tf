4ed7231b3ee1cea32b810e62cdc86d13408a894c
rajubandi <rajubandi@gmail.com>
2026-03-31 21:44:59 +0530

    bnr.tf file created

---
+++ bnr.tf
@@ -0,0 +1,5 @@
+resource "azurerm_ai_foundry" "name" {
+  name = "power-ai-foundry"
+    resource_group_name = azurerm_resource_group.power.name
+    location = azurerm_resource_group.power.location
+}
